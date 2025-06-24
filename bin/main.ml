bd240fd02d94dfb547a8b81b18c87afa2cd00e83
JoaoPaulo-creator <joaosarto@gmail.com>
2025-06-23 21:11:10 -0300

    fix: ajustado imports ao modulo principal

--- bin/main.ml
+++ bin/main.ml
@@ -1,6 +1,5 @@
 open Expr
 open Token
-open Ast_printer
 open Interpreter
 open Environment
 
@@ -9,14 +8,14 @@ let () =
     Binary (
       Unary (
         { token_type = Minus; lexeme = "-"; literal = None; line = 1 },
-        Literal (lit_number 123.0)
+        Literal (Lit_number 123.0)
       ),
       { token_type = Star; lexeme = "*"; literal = None; line = 1 },
-      Grouping (Literal (lit_number 45.67))
+      Grouping (Literal (Lit_number 45.67))
     )
   in
 
   let env = Env.create () in
   let result = evaluate expr env in
-  Printf.printf "Ast: %s\n" (Ast_printer.print expr);
+  (* Printf.printf "Ast: %s\n" (Ast_printer.print expr); *)
   Printf.printf "Evaluated: %s\n" (stringify result)
